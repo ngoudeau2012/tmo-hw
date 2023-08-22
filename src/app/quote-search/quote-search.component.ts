07c6f64a614659bf06fd2589e89572654e3fe679
ngoodo <ngoudeau2012@gmail.com>
2023-08-22 18:12:00 -0500

    added more comments to code

--- src/app/quote-search/quote-search.component.ts
+++ src/app/quote-search/quote-search.component.ts
@@ -1,36 +1,37 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { Quote, SearchResponse } from '../models/quote.model';
 import { QuoteService } from '../services/quote.service';
 
 @Component({
-    selector: 'app-quote-search',
-    templateUrl: './quote-search.component.html',
-    styleUrls: ['../app.component.css']
+  selector: 'app-quote-search',
+  templateUrl: './quote-search.component.html',
+  styleUrls: ['../app.component.css'],
 })
-
 export class QuoteSearchComponent implements OnInit {
-    searchControl = new FormControl;
+  // set up
+  searchControl = new FormControl();
 
-    quotes: Quote[] = [];
+  // set up an array to put the quotes from the response
+  quotes: Quote[] = [];
 
-    constructor(private quoteService: QuoteService){}
-    
-    ngOnInit(): void {
-        // track the users input on change in the input field
-        this.searchControl.valueChanges.subscribe(searchTerm => {
-            // if there is a search term, call the api and return the data or error
-            if(searchTerm){
-                this.quoteService.searchQuotes(searchTerm).subscribe({
-                    next: (data: SearchResponse) => this.quotes = data.results,
-                    error: error => console.error('Error getting quotes:', error)
-                }
-                   
-                );
-            }else {
-                // Clear the quotes if the search term is empty
-                this.quotes = [];  
-            }
+  // bring in the quoteService as a dependency
+  constructor(private quoteService: QuoteService) {}
+
+  ngOnInit(): void {
+    // track the users input on change in the input field
+    this.searchControl.valueChanges.subscribe((searchTerm) => {
+      // if there is a search term, call the api and return the data or error
+      if (searchTerm) {
+        this.quoteService.searchQuotes(searchTerm).subscribe({
+          // add the data.results array to the quotes array
+          next: (data: SearchResponse) => (this.quotes = data.results),
+          error: (error) => console.error('Error getting quotes:', error),
         });
-    }
+      } else {
+        // Clear the quotes if the search term is empty
+        this.quotes = [];
+      }
+    });
+  }
 }
